0cc422d759a4a5687bcfd7b9ec36b0fab8bf54c3
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2025-11-15 18:01:03 +0530

    add admin routes (users list, todos list, update, delete)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { clerkClient } from "@clerk/nextjs/server";
+
 export const validateEmail = (email: string) => {
   return email.match(
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -6,4 +8,14 @@ export const validateEmail = (email: string) => {
 
 export const validateFullName = (name: string) => {
   return name.match(/^[A-Za-z\s]{8,}$/);
+};
+
+export const isAdmin = async (userId: string) => {
+  try {
+    const client = await clerkClient();
+    const user = await client.users.getUser(userId);
+    return user.publicMetadata.role === "admin";
+  } catch (error) {
+    throw error;
+  }
 };

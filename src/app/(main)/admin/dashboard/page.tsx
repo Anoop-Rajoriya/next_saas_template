ac5eaa69ff5407529e2d9214110c466674dce06b
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2025-11-25 10:11:01 +0530

    Clean slate: remove all old project files

--- src/app/(main)/admin/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function AdminDashboard() {
-  return <div className="flex-1">AdminDashboard</div>;
-}
-
-export default AdminDashboard;
--- src/app/(main)/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-function Dashboard() {
-  return <div className="grid grid-cols-1 lg:grid-cols-3 gap-8"></div>;
-}
-
-export default Dashboard;

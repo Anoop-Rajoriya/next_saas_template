db65c10f683bc1667a511687526386623272199c
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2025-12-26 20:05:59 +0530

    Refactor useSignin and useSignup hooks to simplify error handling and loading states

--- app/(components)/index.ts
+++ app/(components)/index.ts
@@ -6,8 +6,21 @@ import TodoList from "./feature/TodoList";
 
 import Checkbox from "./ui/Checkbox";
 import Logo from "./ui/Logo";
+import Input from "./ui/Input";
+import Button from "./ui/Button";
+import Form from "./ui/Form";
 
 import type { Todo } from "./type";
 
-export { Container, ThemeToggle, TodoInput, TodoList, Checkbox, Logo };
+export {
+  Container,
+  ThemeToggle,
+  TodoInput,
+  TodoList,
+  Checkbox,
+  Logo,
+  Input,
+  Button,
+  Form,
+};
 export type { Todo };
--- hooks/useSignin.ts
+++ hooks/useSignin.ts
@@ -1,36 +1,24 @@
-import parseClerkErrors, { Errors } from "@/lib/parseClerkErrors";
 import { useSignIn } from "@clerk/nextjs";
-import { useState } from "react";
 
 function useSignin() {
   const { isLoaded, setActive, signIn } = useSignIn();
-  const [isLoading, setIsLoading] = useState(false);
-  const [errors, setErrors] = useState<Errors>({});
 
   const signin = async (identifier: string, password: string) => {
-    if (!isLoaded || isLoading) return;
-    try {
-      setIsLoading(true);
-      setErrors({});
-      const signinAttempt = await signIn.create({
-        identifier,
-        password,
-      });
+    if (!isLoaded) return;
+    const signinAttempt = await signIn.create({
+      identifier,
+      password,
+    });
 
-      if (signinAttempt.status === "complete") {
-        await setActive({ session: signinAttempt.createdSessionId });
-      } else {
-        console.warn("Sign in attempt failed, status: ", signinAttempt.status);
-        throw new Error("Failed to sign in. Retry after some time.");
-      }
-    } catch (error) {
-      setErrors(parseClerkErrors(error));
-    } finally {
-      setIsLoading(false);
+    if (signinAttempt.status === "complete") {
+      await setActive({ session: signinAttempt.createdSessionId });
+    } else {
+      console.warn("Sign in attempt failed, status: ", signinAttempt.status);
+      throw new Error("Failed to sign in. Retry after some time.");
     }
   };
 
-  return { signin, isLoading, errors };
+  return { signin, isLoaded };
 }
 
 export default useSignin;
